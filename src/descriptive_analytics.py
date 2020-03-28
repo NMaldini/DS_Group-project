0ab3307f2a9a2a777d57ea23f52d68247457cb3c
Akalanka Weerasooriya <mailboxaaw@gmail.com>
2020-03-28 16:53:24 +0530

    Descriptive analytics

--- src/descriptive_analytics.py
+++ src/descriptive_analytics.py
@@ -7,8 +7,8 @@ df = pd.read_excel(DATA_SET_LOCATION)
 
 pd.set_option('display.max_columns', 36)
 
-# print('Attribute types: \n' + str(df.dtypes) + '\n')
-# print('Description of data: \n' + str(df.describe()))
+print('Attribute types: \n' + str(df.dtypes) + '\n')
+print('Description of data: \n' + str(df.describe()))
 
 hist = df.hist(bins=10)
 plt.subplots_adjust(hspace=0.8)
